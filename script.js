619bf1e294da01b9f7183dd93a16645216df1704
Tetianakryvobok <tatiana.lv@ukr.net>
2023-02-24 13:25:37 +0200

    lesson-21

---
+++ script.js
@@ -0,0 +1,15 @@
+let firstnumber;
+let secondnumber;
+
+firstnumber = prompt("Enter first number");
+secondnumber = prompt("Enter second number");
+
+
+let sum = Number(firstnumber) + Number(secondnumber);
+let difference = Number(firstnumber) - Number(secondnumber);
+let product = Number(firstnumber) * Number(secondnumber);
+let quotient = Number(firstnumber) / Number(secondnumber);
+
+alert("Sum: " + sum + "\ndifference: " + difference + "\nproduct: " + product + "\nquotient: " + quotient);
+
+
