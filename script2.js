b38a53583c50e5c7d9976b576576f1c93a4ea4f5
Tetianakryvobok <tatiana.lv@ukr.net>
2023-03-24 12:05:50 +0200

    lesson23

---
+++ script2.js
@@ -0,0 +1,30 @@
+let arr = [74, 12, 27, 37, 41, 108, 36, 7, 15, 73, 3, 'test'];
+
+let sum = 0;
+let min = arr[0];
+let max = arr[0];
+
+for (let i = 0; i < arr.length; i++) {
+
+   if (typeof arr[i] === "number") {
+      sum = sum + arr[i];
+
+      if (arr[i] < min) {
+         min = arr[i];
+      }
+      if (arr[i] > max) {
+         max = arr[i];
+      }
+   }
+}
+console.log(sum);
+console.log(min);
+console.log(max);
+
+for (let i = 1; i <= 5; i++) {
+   let row = "";
+   for (let n = 1; n <= i; n++) {
+      row += "#";
+   }
+   console.log(row);
+}
